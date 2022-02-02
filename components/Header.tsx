30a39478e3a409b2fb46e1ae3c1c5845c7def93c
Tino Liu <justinoliu.work@gmail.com>
2022-02-02 11:49:14 +0800

    Header checkpoint

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../public/blacklogo.png";
+
 function Header() {
   return (
-    <div>
-      <h1>I am a header</h1>
-    </div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <Image
+            src={Logo}
+            width={100}
+            height={60}
+            className="object-contain cursor-pointer"
+          />
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="text-white bg-blue-500 px-4 py-1 rounded-full">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">
+          Get Started
+        </h3>
+      </div>
+    </header>
   );
 }
 
